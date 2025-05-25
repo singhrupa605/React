e9087c7f46b2aedd3d8768af3433e5b732191ead
Rupa Singh <singhrupa605@gmail.com>
2025-05-25 17:31:35 +0530

    learning parcel

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 /** <div id="parent">
-  <div id="child">
-      <h1>I am a child</h1>
-  </div>
+<div id="child">
+  <h1>I am a child</h1>
+</div>
 </div> */
 
 const parent = React.createElement("div", { id: "parent" },
     React.createElement("div", { id: "child" },
 
-        [React.createElement("h1", {}, "I am a child"), React.createElement("h2", {}, "I am a child two")]
+        [React.createElement("h1", {}, "I AM FIRST"), React.createElement("h2", {}, "I am a child two")]
 
     ));
 
+
+
+
 //const heading = React.createElement("h1", { id: "heading", style: { color: "red" } }, "Hello World From React"); //Creates Object or react element
 const root = ReactDOM.createRoot(document.getElementById("root"))
-
-console.log(parent)
 
 root.render(parent);  //takes object and creates an html element 
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Namaste React 📝
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm written in C++
+- Caching - subsequent builds take lesser time
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Content Hashing
+- Differential Bundling
+- Code splitting
+- Diagnostic
+- Different Build for Dev and Prod
+- HTTPs
+- Error Handling
